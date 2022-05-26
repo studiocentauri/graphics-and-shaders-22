5a5a867bcf18185a1bcda9b02dbca2f930e34f2f
TheTUFGuy <ppmall20@iitk.ac.in>
2022-05-27 02:03:01 +0530

    Add glfw library

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "Config.h"
+#include "GLFW/glfw3.h"
 
 int main()
 {
+    glfwInit();
     std::cout<<WINDOW_NAME<<std::endl;
+    glfwTerminate();
     return 0;
 }
 
