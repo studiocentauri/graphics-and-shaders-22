86000df985e3d9b7c54212d255398e173eee46e4
TheTUFGuy <ppmall20@iitk.ac.in>
2022-05-27 01:01:44 +0530

    Add GitIgnore and basic working Cmake

---
+++ includes/Config.h
@@ -0,0 +1,5 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#define WINDOW_NAME "Graphics And Shaders"
+
+#endif
---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Config.h"
+
+int main()
+{
+    std::cout<<WINDOW_NAME<<std::endl;
+    return 0;
+}
+
